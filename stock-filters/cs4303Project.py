5ef84a534f058cc53d49cc4261d2415414c619a2
delightedwaif <tim@milkmoovement.com>
2020-01-27 11:13:17 -0330

    Add filter file

---
+++ stock-filters/cs4303Project.py
@@ -0,0 +1,42 @@
+from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
+
+inputs = (
+    ('cs4303', 'label'),
+    ('Material1', alphaMaterials.StoneBricks),
+    ('Density', (10, 1, 100)),
+    ('Chance', 10),
+    ('Creator: ChirpNets', 'label'),
+)
+
+
+def perform(level, box, options):
+    print("Hello")
+
+
+def construct_building():
+    # This is where we build a building
+    pass
+
+
+def construct_floor():
+    # This is where we build a floor
+    pass
+
+
+def construct_pillars():
+    # This is where we build pillars
+    pass
+
+
+def construct_walls():
+    # build walls
+    pass
+
+
+def construct_roof():
+    # build roof?
+    pass
+
+
+def build_paths():
+    pass
