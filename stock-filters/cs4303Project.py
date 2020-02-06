f86d4053f02adcc5e9fa441d22f14a462aa9f642
delightedwaif <tim@milkmoovement.com>
2020-02-06 13:00:58 -0330

    adding windows

--- stock-filters/cs4303Project.py
+++ stock-filters/cs4303Project.py
@@ -15,7 +15,7 @@ def perform(level, box, options):
     construct_floor(level, box)
 
 
-def construct_building(level, box):
+def construct_building(level, box, type):
     # This is where we build a building
     pass
 
@@ -37,9 +37,16 @@ def construct_walls(level, box, details):
     for x in range(box.minx, box.maxx):
         for z in range(box.minz, box.maxz):
             for y in range(box.miny, box.miny+details['height']):
-                if (x == box.maxx-1 or z == box.maxz-1 or x == box.minx or z == box.minz):
+                if x == box.maxx-1 or z == box.maxz-1 or x == box.minx or z == box.minz:
+                    if (y % 3 == 2):
+                        utilityFunctions.setBlock(
+                            level, (160, 3), x, y, z)
+                    else:
+                        utilityFunctions.setBlock(
+                            level, details['block'], x, y, z)
+                elif level.blockAt(x, y, z) != 0:
                     utilityFunctions.setBlock(
-                        level, details['block'], x, y, z)
+                        level, (0, 0), x, y, z)
 
 
 def construct_roof(level, box, details):
@@ -47,7 +54,8 @@ def construct_roof(level, box, details):
     # build roof
     for x in range(box.minx, box.maxx):
         for z in range(box.minz, box.maxz):
-            utilityFunctions.setBlock(level, details['block'], x, box.miny+details['height'], z)
+            utilityFunctions.setBlock(
+                level, details['block'], x, box.miny+details['height'], z)
 
 
 def build_paths(level, box):
