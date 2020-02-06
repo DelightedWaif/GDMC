7f1736a6469ea6065f47490cf0fe81164b6727b3
delightedwaif <tim@milkmoovement.com>
2020-02-06 12:27:40 -0330

    adding simple structure code

--- stock-filters/cs4303Project.py
+++ stock-filters/cs4303Project.py
@@ -1,42 +1,55 @@
 from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
+import utilityFunctions
 
 inputs = (
     ('cs4303', 'label'),
     ('Material1', alphaMaterials.StoneBricks),
-    ('Density', (10, 1, 100)),
-    ('Chance', 10),
     ('Creator: ChirpNets', 'label'),
 )
 
 
 def perform(level, box, options):
-    print("Hello")
+    wall_details = {'height': 5, 'block': (98, 0)}
+    construct_walls(level, box, wall_details)
+    construct_roof(level, box, wall_details)
+    construct_floor(level, box)
 
 
-def construct_building():
+def construct_building(level, box):
     # This is where we build a building
     pass
 
 
-def construct_floor():
+def construct_floor(level, box):
     # This is where we build a floor
-    pass
+    for x in range(box.minx, box.maxx):
+        for z in range(box.minz, box.maxz):
+            utilityFunctions.setBlock(level, (98, 0), x, box.miny, z)
 
 
-def construct_pillars():
+def construct_pillars(level, box):
     # This is where we build pillars
     pass
 
 
-def construct_walls():
+def construct_walls(level, box, details):
     # build walls
-    pass
+    for x in range(box.minx, box.maxx):
+        for z in range(box.minz, box.maxz):
+            for y in range(box.miny, box.miny+details['height']):
+                if (x == box.maxx-1 or z == box.maxz-1 or x == box.minx or z == box.minz):
+                    utilityFunctions.setBlock(
+                        level, details['block'], x, y, z)
 
 
-def construct_roof():
-    # build roof?
-    pass
+def construct_roof(level, box, details):
+
+    # build roof
+    for x in range(box.minx, box.maxx):
+        for z in range(box.minz, box.maxz):
+            utilityFunctions.setBlock(level, details['block'], x, box.miny+details['height'], z)
 
 
-def build_paths():
+def build_paths(level, box):
+    # this is where we build paths
     pass
