16b7028cba0a952ed88b1b5db2f62f7691bc6e1f
delightedwaif <tim@milkmoovement.com>
2020-02-12 11:49:04 -0330

    adding buildings folder and basic buildings class

---
+++ stock-filters/CS4303_Buildings/basic_building.py
@@ -0,0 +1,38 @@
+from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
+import utilityFunctions
+
+class BasicBuilding():
+    def __init__(self, level, box, block_id=98, block_type=0, height=10):
+        details = {'height': height, 'block': (block_id, block_type)}
+        self.construct_walls(level, box, details)
+        self.construct_roof(level, box, details)
+        self.construct_floor(level, box)
+
+    def construct_walls(self, level, box, details):
+        # build walls
+        for x in range(box.minx, box.maxx):
+            for z in range(box.minz, box.maxz):
+                for y in range(box.miny, box.miny+details['height']):
+                    if x == box.maxx-1 or z == box.maxz-1 or x == box.minx or z == box.minz:
+                        if (y % 3 == 2):
+                            utilityFunctions.setBlock(
+                                level, (160, 3), x, y, z)
+                        else:
+                            utilityFunctions.setBlock(
+                                level, details['block'], x, y, z)
+                    elif level.blockAt(x, y, z) != 0:
+                        utilityFunctions.setBlock(
+                            level, (0, 0), x, y, z)
+
+    def construct_roof(self, level, box, details):
+        # build roof
+        for x in range(box.minx, box.maxx):
+            for z in range(box.minz, box.maxz):
+                utilityFunctions.setBlock(
+                    level, details['block'], x, box.miny+details['height'], z)
+
+    def construct_floor(self, level, box):
+        # This is where we build a floor
+        for x in range(box.minx, box.maxx):
+            for z in range(box.minz, box.maxz):
+                utilityFunctions.setBlock(level, (98, 0), x, box.miny, z)
--- stock-filters/cs4303Project.py
+++ stock-filters/cs4303Project.py
@@ -1,5 +1,6 @@
 from pymclevel import alphaMaterials, MCSchematic, MCLevel, BoundingBox
 import utilityFunctions
+from CS4303_Buildings.basic_building import BasicBuilding
 
 inputs = (
     ('cs4303', 'label'),
@@ -9,10 +10,7 @@ inputs = (
 
 
 def perform(level, box, options):
-    wall_details = {'height': 5, 'block': (98, 0)}
-    construct_walls(level, box, wall_details)
-    construct_roof(level, box, wall_details)
-    construct_floor(level, box)
+    BasicBuilding(level, box)
 
 
 def construct_building(level, box, type):
@@ -20,42 +18,9 @@ def construct_building(level, box, type):
     pass
 
 
-def construct_floor(level, box):
-    # This is where we build a floor
-    for x in range(box.minx, box.maxx):
-        for z in range(box.minz, box.maxz):
-            utilityFunctions.setBlock(level, (98, 0), x, box.miny, z)
-
-
 def construct_pillars(level, box):
     # This is where we build pillars
     pass
-
-
-def construct_walls(level, box, details):
-    # build walls
-    for x in range(box.minx, box.maxx):
-        for z in range(box.minz, box.maxz):
-            for y in range(box.miny, box.miny+details['height']):
-                if x == box.maxx-1 or z == box.maxz-1 or x == box.minx or z == box.minz:
-                    if (y % 3 == 2):
-                        utilityFunctions.setBlock(
-                            level, (160, 3), x, y, z)
-                    else:
-                        utilityFunctions.setBlock(
-                            level, details['block'], x, y, z)
-                elif level.blockAt(x, y, z) != 0:
-                    utilityFunctions.setBlock(
-                        level, (0, 0), x, y, z)
-
-
-def construct_roof(level, box, details):
-
-    # build roof
-    for x in range(box.minx, box.maxx):
-        for z in range(box.minz, box.maxz):
-            utilityFunctions.setBlock(
-                level, details['block'], x, box.miny+details['height'], z)
 
 
 def build_paths(level, box):
