e84ede68cd4abee5118986bf7b3fe3f0075e2dea
Spencer Axford <saxford@mun.ca>
2020-04-11 23:10:54 -0230

    Removed "material" from settings keys and added newline

--- stock-filters/ChirpVillage/BiomeSettings.py
+++ stock-filters/ChirpVillage/BiomeSettings.py
@@ -3,15 +3,15 @@
 # block id's can be found at https://minecraft-ids.grahamedgecombe.com/
 biomeSettings = {
 	"Plains": { # Default
-        "wall_material": (5, 0), # Oak Planks
-        "fence_material": (85, 0), # Oak Fence
-        "floor_material": (98,0), # Stone Brick
-        "road_material": (4,0) # Cobblestone
+        "wall": (5, 0), # Oak Planks
+        "fence": (85, 0), # Oak Fence
+        "floor": (98,0), # Stone Brick
+        "road": (4,0) # Cobblestone
 	},
 	"Desert": {
-        "wall_material": (24, 1), # Chiseled Sandstone
-        "fence_material": (192, 0), # Acacia Fence
-        "floor_material": (24,0), # Sandstone
-        "road_material": (24,2) # Smooth Sandstone
+        "wall": (24, 1), # Chiseled Sandstone
+        "fence": (192, 0), # Acacia Fence
+        "floor": (24,0), # Sandstone
+        "road": (24,2) # Smooth Sandstone
 	}
 }
